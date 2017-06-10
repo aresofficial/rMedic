e48a67a8fb46e490e98bdeb165d8a615ee6d0108
Vladimir Rets <vladimir.rets@gmail.com>
2017-06-10 18:04:55 +0300

    Add directory structure

--- rMedic/Views/MainWindow.xaml.cs
+++ rMedic/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace rMedic
+namespace rMedic.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
